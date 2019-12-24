08d5af3f6ff49b78f6e2d1e526e3e70cfec3572e
vinod singh rawat <vdvinodrawat@gmail.com>
2019-12-25 00:06:57 +0530

    new style

--- src/Components/Home/nav.js
+++ src/Components/Home/nav.js
@@ -1,6 +1,14 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 class Nav extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isSignin : true};
+    this.changeSwitch = this.changeSwitch.bind(this);
+  }
+  changeSwitch(event){
+    this.setState({isSignin : !this.state.isSignin});
+  }
     render (){
         return <div className="main-nav">
 
@@ -9,7 +17,7 @@ class Nav extends React.Component {
           <li></li>
           <li></li>
           <li></li>
-          <li><Link className="signInUp" to={"/signIn"}>Sign In</Link><Link className="signInUp" to={"/signUp"}>Sign Up</Link></li>
+          <li><Link className={this.state.isSignin?"signInUp fadeIn":"signInUp fadeOut"} style={{display:this.state.isSignin ? "inline-block":"none"}} onClick={this.changeSwitch} to={"/signIn"}>Sign In</Link><Link className={this.state.isSignin?"signInUp fadeOut":"signInUp fadeIn"}style={{display:this.state.isSignin ? "none":"inline-block"}} to={"/signUp"} onClick={this.changeSwitch}>Sign Up</Link></li>
         
         </ul>
       </div>
