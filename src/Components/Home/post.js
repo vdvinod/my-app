2074841699c06f19b1279fd9f8602d6b6e420c83
Vinod <vdvinodrawat@gmail.com>
2020-01-02 19:07:26 +0530

    no of users liked post

--- src/Components/Home/post.js
+++ src/Components/Home/post.js
@@ -13,7 +13,8 @@ class CommentList extends React.Component {
         this.state.likesCount = "";
         this.reply ="";
         this.previousUlId = {};
-        this.state.isLiked = {}
+        this.state.isLiked = {};
+        this.state.likeDislikeList = [];
     }
     saveLike = (index, notFromButton)=>{
         let likeId = this.props.commentList[index].Likes.indexOf(this.userData.userId)
@@ -79,9 +80,8 @@ class CommentList extends React.Component {
              }});
     }
     repeatList=()=>{
-        console.log(this.state.reply);
         this.li = this.props.commentList.map((element,key) => {
-             this.state.repliesLi = element.replies.map((reply,key)=>{
+                this.repliesLi = element.replies.map((reply,key)=>{
                 var userName = JSON.parse(localStorage.getItem("userList")).find((val) => {
                     if(val.userId === reply.userId){
                         return val;
@@ -98,6 +98,7 @@ class CommentList extends React.Component {
                     
                 </li>)
              });
+            /// this.setState({repliesLi : repliesList});
             var userName = JSON.parse(localStorage.getItem("userList")).find((val) => {
                 if(val.userId === element.userId){
                     return val;
@@ -110,8 +111,8 @@ class CommentList extends React.Component {
                     </div>
                     <div className="post-s">
                         {element.comment}
-                        <div>
-                            <button className="btnLikeDislike" onClick={()=>this.saveLike(key)}> 
+                        <div className="likeDislikes">
+                            <button className="btnLikeDislike" onClick={()=>this.saveLike(key)} onMouseOver={(event)=>this.showLikeDislikeList(event,key,'Likes')}> 
                                 <span style={{display:(element.Likes.indexOf(this.userData.userId)===-1)?"inline":"none"}}>Likes</span> 
                                 <span style={{display:(element.Likes.indexOf(this.userData.userId)>-1)?"inline":"none"}}>Liked</span>
                                 <span style={{display:(element.Likes.length?"inline":"none")}}> {element.Likes.length}</span></button>
@@ -120,13 +121,18 @@ class CommentList extends React.Component {
                                 <span style={{display:(element.dislikes.indexOf(this.userData.userId)>-1)?"inline":"none"}}>DisLiked</span>
                                 <span style={{display:(element.dislikes.length?"inline":"none")}}> {element.dislikes.length}</span></button>
                             <button className="btnLikeDislike" onClick={()=>this.showReplyBox(key)}>comment</button>
+                            <div className="likeDislikeList" id="likeDislikeList">
+                                <ul>
+                                    {this.likeDislikeList}
+                                </ul>
+                            </div>
                         </div>
                         <div>
                             {this.state.replyBox[key]}
                         </div>
                         <div className="replyUl">
                             <ul id={'reply'+key}>
-                                {this.state.repliesLi}
+                                {this.repliesLi}
                             </ul>
                             {(element.replies.length > 2) && <div className="showMore" onClick={()=>this.showMore(key,'showMore'+element.commentId)} id={'showMore'+element.commentId}>Show All Comments</div>}
                         </div>
@@ -134,6 +140,21 @@ class CommentList extends React.Component {
                     </div>
                 </li>
         });
+    }
+
+    showLikeDislikeList = (event, key,list) => {
+        
+        this.likeDislikeList = this.props.commentList[key][list].map((value,k) =>{
+           let listLD = JSON.parse(localStorage.getItem("userList")).find((val)=>{
+                return val.userId === value;    
+            });
+            return <li key={k}>{listLD.firstName} {listLD.lastName}</li>;
+        });
+        document.getElementById("likeDislikeList").style = {
+            display : 'block',
+            top: event.clientX,
+            left: event.clientY
+        }
     }
     render(){
         this.repeatList();
